f9f016d9713933d08d0e9c5d3feba2598f62939f
MarkiMark01 <markishgroup@gmail.com>
2023-11-26 13:15:02 +0200

    create cast page

---
+++ src/components/pages/Cast/Cast.js
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getMovieCast } from "../../services/fetchMovies";
+
+const MovieCast = () => {
+    const [movieCast, setMovieCast] = useState([]);
+    const { id: movieId } = useParams();
+
+    useEffect(() => {
+        const movieCastFetch = async () => {
+            try {
+                const data = await getMovieCast(movieId);
+                setMovieCast(data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        if (movieId) {
+            movieCastFetch();
+        }
+    }, [movieId]);
+
+    const { cast } = movieCast;
+
+    const element = cast?.map(({ cast_id, name, character, profile_path }) => (
+        <li key={cast_id}>
+            {profile_path ? (
+                <img
+                    src={`https://image.tmdb.org/t/p/w185${profile_path}`}
+                    alt={name}
+                />
+            ) : (
+                <img
+                    src={
+                        "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"
+                    }
+                    alt={name}
+                    width={185}
+                />
+            )}
+            <div>
+                <p>{name}</p>
+                <p>
+                    <span>as</span> {character}
+                </p>
+            </div>
+        </li>
+    ));
+    return cast && cast.length > 0 ? (
+        <ul>{element}</ul>
+    ) : (
+        <p>Unfortunately, there is no information about the actors yet...</p>
+    );
+};
+
+export default MovieCast;
