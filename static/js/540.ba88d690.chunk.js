d26a22c993bb5c48cf12ecddc58c84325e85864c
MarkiMark01 <markishgroup@gmail.com>
2024-01-28 15:21:14 +0200

    Updates

---
+++ static/js/540.ba88d690.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_router=self.webpackChunkreact_router||[]).push([[540],{540:(e,r,a)=>{a.r(r),a.d(r,{default:()=>n});var s=a(87),c=a(184);const n=()=>(0,c.jsxs)("div",{className:"container",children:[(0,c.jsx)("h2",{children:"Page not found..."}),(0,c.jsx)(s.rU,{to:"/",children:"To main page"})]})}}]);
+//# sourceMappingURL=540.ba88d690.chunk.js.map
